fff559de093e11c6ba2c69837bfc427c6d18968d
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2024-10-11 16:28:02 +0100

    add extra columns to the users table

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
     ms_id = Column(String, unique=True, nullable=False)
     public_key = Column(String, nullable=False)
     private_key = Column(String, nullable=False)
