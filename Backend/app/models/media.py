df2fdb3a7869020e150c9791befe354166358336
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2024-10-13 22:11:27 +0100

    update the media table for more encryption

--- Backend/app/models/media.py
+++ Backend/app/models/media.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func, LargeBinary
 from app.db.connection import Base
 
 class Media(Base):
     __tablename__ = 'media'
 
     id = Column(Integer, primary_key=True, index=True)
-    message_id = Column(Integer, ForeignKey('messages.id'), nullable=False)
-    encrypted_file_path = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    filename = Column(String, nullable=False)
+    mime_type = Column(String, nullable=False)
+    size = Column(Integer, nullable=False)
+    encrypted_content = Column(LargeBinary, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    message_id = Column(Integer, ForeignKey('messages.id'), nullable=True)
